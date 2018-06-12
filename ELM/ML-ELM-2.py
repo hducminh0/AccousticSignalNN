6ea8a6d4abf1c1c543ab2ffacaca07b3cbdcd467
hducminh0 <hduc.minhlp@gmail.com>
2018-06-12 09:09:02 +0200

    Add activated random mapping layer b4 mlelm

--- ELM/ML-ELM-2.py
+++ ELM/ML-ELM-2.py
@@ -17,30 +17,30 @@ print('prepare time: ', end - start)
 print('start training')
 start = time.time()
 np.random.seed(seed = None)
-
+# random mapping
+rm, r = np.linalg.qr(np.random.normal(size = (training['signal'].shape[1], 100)))	# randomize input weights of h0
+training['signal'] = hidden(training['signal'], rm)
 # first hidden layer h0 - autoencoder elm-ae
-w0_in = np.random.normal(size = (training['signal'].shape[1], 1200))	# randomize input weights of h0
+w0_in = np.random.normal(size = (training['signal'].shape[1], 5000))	# randomize input weights of h0
 w0_in, r = np.linalg.qr(w0_in)	# make orthogonal weight vectors
 h0 = hidden(training['signal'], w0_in)		# output of h0
 w0_out = np.matmul(np.linalg.pinv(h0), training['signal'])	# tune output weight of h0
 training['signal'] = np.matmul(h0, w0_out)	# the input signal after autoencoder
-
 # second hidden layer h1 - elm
 w1_in = np.random.normal(size = (training['signal'].shape[1], 4100))	# randomize input weights of h1
 h1 = hidden(training['signal'], w1_in)		# output of h1
 # w1_out = np.linalg.lstsq(h, training['thickness'], rcond = None)[0]		# use least square to find the optimal output weight 
 w1_out = np.matmul(np.linalg.pinv(h1), training['thickness'])	# tune output weight for h1
 end = time.time()
-
 print('training time: ', end - start)
 
 print('start testing')
 start = time.time()
-
+# random mapping
+testing['signal'] = hidden(testing['signal'], rm)
 # h0
 h0 = hidden(testing['signal'], w0_in)	# output of h0
 testing['signal'] = np.matmul(h0, w0_out)		# after autoencoder
-
 # h1
 h1 = hidden(testing['signal'], w1_in)
 approx = np.matmul(h1, w1_out)
@@ -50,10 +50,12 @@ end = time.time()
 print('testing time: ', end - start)
 
 # save the network for future use 
-sio.savemat('network.mat', {'w0_in': w0_in, 'w0_out': w0_out, 'w1_in': w1_in, 'w1_out': w1_out, 'mean': m, 'n': n})
+sio.savemat('network.mat', {'rm': rm, 'w0_in': w0_in, 'w0_out': w0_out, 'w1_in': w1_in, 'w1_out': w1_out, 'mean': m, 'n': n})
 
 # plot the first 100 samples 
 print('plot')
+approx = approx * n + m
+testing['thickness'] = testing['thickness'] * n + m
 plot_model(approx, testing['thickness'])
 
 ###########################################################################################
@@ -61,18 +63,21 @@ plot_model(approx, testing['thickness'])
 # n_nodes = 0	# number of node in the hidden layer
 # error = np.array([[]])	# initialize matrix to store all the mse 
 
-# while n_nodes < 4100:
+# while n_nodes < 7000:
 # 	# start training the model
 # 	n_nodes += 100
 # 	start = time.time()
-# 	w0_in = np.random.normal(size = (training['signal'].shape[1], n_nodes))	# randomize input weights of the network 
+# 	# random mapping
+# 	rm, r = np.linalg.qr(np.random.normal(size = (training['signal'].shape[1], 100)))	# randomize input weights of h0
+# 	training['signal'] = hidden(training['signal'], rm)
+# 	w0_in = np.random.normal(size = (training['signal'].shape[1], 100))	# randomize input weights of the network 
 # 	w0_in, r = np.linalg.qr(w0_in)
 # 	# h = hidden(training['signal'], input_w)		# output of the hidden layer
 # 	h0 = hidden(training['signal'], w0_in)		# output of the hidden layer
 # 	w0_out = np.matmul(np.linalg.pinv(h0), training['signal'])
 
 # 	training['signal'] = np.matmul(h0, w0_out)
-# 	w1_in = np.random.normal(size = (training['signal'].shape[1], 4100))	# randomize input weights of the network 
+# 	w1_in = np.random.normal(size = (training['signal'].shape[1], n_nodes))	# randomize input weights of the network 
 # 	h1 = hidden(training['signal'], w1_in)		# output of the hidden layer
 # 	# w1_out = np.linalg.lstsq(h, training['thickness'], rcond = None)[0]		# use least square to find the optimal output weight 
 # 	w1_out = np.matmul(np.linalg.pinv(h1), training['thickness'])
@@ -80,7 +85,8 @@ plot_model(approx, testing['thickness'])
 # 	print('training time ', n_nodes/100, ': ', end - start)
 
 # 	# start testing
-# 	start = time.time()
+# 	start = time.time()# random mapping
+# 	testing['signal'] = hidden(testing['signal'], rm)
 # 	h0 = hidden(testing['signal'], w0_in)	# output of the hidden layer
 # 	testing['signal'] = np.matmul(h0, w0_out)		# approximated values
 # 	h1 = hidden(testing['signal'], w1_in)
